ce12bd4497872871def92d8c73739b56e6b1fc90
Chad Paik <chad.paik@voyagerx.com>
2019-12-30 01:09:10 -0500

    added camera device manager

--- app/src/main/java/com/chaddysroom/vloggingapp/MainActivity.kt
+++ app/src/main/java/com/chaddysroom/vloggingapp/MainActivity.kt
@@ -11,6 +11,7 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.HandlerThread
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
@@ -20,15 +21,70 @@ import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var backgroundThread: HandlerThread
+    private lateinit var backgroundHandler: Handler
+    private lateinit var cameraDevice: CameraDevice
+
     companion object {
         const val REQUEST_CAMERA_PERMISSION = 100
         const val TAG = "MainActivity"
     }
 
 
+    private fun startBackgroundThread() {
+        backgroundThread = HandlerThread("Camera2").also { it.start() }
+        backgroundHandler = Handler(backgroundThread.looper)
+    }
+
+    private fun stopBackgroundThread() {
+        backgroundThread.quitSafely()
+        try {
+            backgroundThread.join()
+        } catch (e: InterruptedException) {
+            Log.e(TAG, e.toString())
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startBackgroundThread()
+
+
+    }
+
+    override fun onPause() {
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+
+    private val deviceStateCallback =
+        object : CameraDevice.StateCallback() { // Object expression, declares anonymous object
+            override fun onOpened(camera: CameraDevice) {
+                Log.d(TAG, "camera device opened")
+                if (camera != null)
+                    cameraDevice = camera
+                else
+                    Log.d(TAG, "Camera is null")
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                Log.d(TAG, "camera deviced disconnected")
+                camera?.close()
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                Log.d(TAG, "camera device error")
+                this@MainActivity.finish()
+            }
+
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startBackgroundThread()
         launchCamera()
     }
 
@@ -42,10 +98,9 @@ class MainActivity : AppCompatActivity() {
         return EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)
     }
 
-
     private fun launchCamera() {
         if (hasCameraPermission()) {
-            Log.d(TAG, "App as camera permission]")
+            Log.d(TAG, "App has camera permission]")
             startCameraSession()
         } else {
             EasyPermissions.requestPermissions(
@@ -57,8 +112,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startCameraSession(){
-        if (hasCameraPermission()) {
+    private fun startCameraSession() {
+        try {
             val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
             if (cameraManager.cameraIdList.isEmpty()) {
                 // no cameras
@@ -66,8 +121,13 @@ class MainActivity : AppCompatActivity() {
             }
             val cameraIdList = cameraManager.cameraIdList
             val firstCamera = cameraIdList[0]
-
-
+            cameraManager.openCamera(firstCamera, deviceStateCallback, backgroundHandler)
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, e.toString())
+        } catch (e: SecurityException) {
+            Log.e(TAG, e.toString())
+        } catch (eƒ: InterruptedException) {
+            Log.e(TAG, "openCamera() interrupted while opened")
         }
 
     }
